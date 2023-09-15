b21dfdfe2eab2fb740254cf2b8e5aea2ea151e09
MSI GL65 <MSI GL65@MSI>
2023-09-14 21:00:32 -0400

    CREACION DE LA ARQUITECTURA, CON SUS CAPAS CREADAS

---
+++ PrivSchool/PrivSchool.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PrivSchool.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
